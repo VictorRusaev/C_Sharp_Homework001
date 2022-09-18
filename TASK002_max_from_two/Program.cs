9363c04cdd1f6d0670c7fc55042a5177dc15d2ac
Victor Rusaev <rusaev74@mail.ru>
2022-09-18 06:44:18 +0300

    TASK002_max_from_two

---
+++ TASK002_max_from_two/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string A = Console.ReadLine();
+int numberA = int.Parse(A);
+
+Console.WriteLine("Введите второе число: ");
+string B = Console.ReadLine();
+int numberB = int.Parse(B);
+
+if(numberB > numberA) Console.WriteLine(numberB);
+else
+{
+    Console.WriteLine(numberA);
+}
