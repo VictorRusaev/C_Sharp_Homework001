611fda38b881deefb94d083030bdfdbed89dcd64
Victor Rusaev <rusaev74@mail.ru>
2022-09-18 08:04:55 +0300

    TASK008_even_line_to_N

---
+++ TASK008_even_line_to_N/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string numberN = Console.ReadLine();
+int N = int.Parse(numberN);
+
+int i = 2;
+
+while(i <= N)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+        i += 2;
+    }
+    
+}
+
