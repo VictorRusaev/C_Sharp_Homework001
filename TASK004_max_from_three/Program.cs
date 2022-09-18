7cde87dba9342ed808891911b4411a2ae9413f43
Victor Rusaev <rusaev74@mail.ru>
2022-09-18 06:51:43 +0300

    TASK004_max_from_three

---
+++ TASK004_max_from_three/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+string A = Console.ReadLine();
+int numberA = int.Parse(A);
+
+Console.WriteLine("Введите второе число: ");
+string B = Console.ReadLine();
+int numberB = int.Parse(B);
+
+Console.WriteLine("Введите третье число: ");
+string C = Console.ReadLine();
+int numberC = int.Parse(C);
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine(max);
