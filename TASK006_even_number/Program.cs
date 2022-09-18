4429b85a14a7d25065d1037c74d06b9fac9869c2
Victor Rusaev <rusaev74@mail.ru>
2022-09-18 06:57:29 +0300

    task006_even_number

---
+++ TASK006_even_number/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string A = Console.ReadLine();
+int numberA = int.Parse(A);
+
+if (numberA % 2 == 0) Console.WriteLine("Чётное");
+else Console.WriteLine("Нечётное");
